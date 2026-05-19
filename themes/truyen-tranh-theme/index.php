b8a78822a03cabdfbe7f69ba36d43f206e4e946a
Nguyendinhduy257 <Nguyendinhduy257@gmail.com>
2026-05-19 17:46:59 +0700

    feat: thêm plugin a3-lazy-load và wp-pagenavi

--- themes/truyen-tranh-theme/index.php
+++ themes/truyen-tranh-theme/index.php
@@ -1,1 +1,15 @@
 <h1>Web Truyện Tranh Đang Khởi Chạy</h1>
+<?php get_header(); ?>
+
+<div class="truyen-list">
+    <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+        <div class="truyen-item">
+            <?php the_post_thumbnail(); ?>
+            <h2><?php the_title(); ?></h2>
+        </div>
+    <?php endwhile; endif; ?>
+</div>
+
+<?php wp_pagenavi(); ?>
+
+<?php get_footer(); ?>
